1fa8a5305f452cd4db15e20ae394952fe3c09e21
prabh-virdi <rajavirdi2001@gmail.com>
2024-09-24 18:30:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+VanillaTilt.init(document.querySelector(".box"), {
+      max: 10,
+      speed: 200,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      reverse: true,
+      glare: true,
+      "max-glare": 0.1,
+    });
